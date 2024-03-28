b1eed1c2f8d853f8ed413543876e1a9be1bcd7b9
Asad Tariq <asad36br@hotmail.com>
2024-03-28 17:14:11 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataGridComponent } from './data-grid/data-grid.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { DxDataGridModule } from 'devextreme-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DataGridComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    DxDataGridModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
